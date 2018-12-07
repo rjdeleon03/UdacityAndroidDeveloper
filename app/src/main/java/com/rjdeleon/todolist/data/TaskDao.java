4e1fe84883be6437bb32211c36d5d84fbc288aca
pabs <pabs@alink-group.co.jp>
2018-12-07 13:04:35 +0800

    Initial commit

---
+++ app/src/main/java/com/rjdeleon/todolist/data/TaskDao.java
@@ -0,0 +1,29 @@
+package com.rjdeleon.todolist.data;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface TaskDao {
+
+    @Query("SELECT * FROM task ORDER BY priority")
+    List<TaskEntry> loadAllTasks();
+
+    @Query("SELECT * FROM task WHERE id = :id")
+    TaskEntry loadTaskById(int id);
+
+    @Insert
+    void insertTask(TaskEntry taskEntry);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateTask(TaskEntry taskEntry);
+
+    @Delete
+    void deleteTask(TaskEntry taskEntry);
+}
